ab5b8e32c4ca2ef1b746bdd8217c8c001053f7e7
Bunziin <bunsin.money@gmail.com>
2023-01-13 16:25:15 +0700

    commit main html

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php include "header.php"; ?>
-
-    <div class="container-fluid mt-3">
-        <h3 class="text-center">Hey guy! Welcome to <b>Bunziin</b> site.</h3>
-        <p class="text-center">welcome site visitors, reply to their questions and encourage them to take some actions.</p>
-    </div>
-    
-<?php include "footer.php";?>    
